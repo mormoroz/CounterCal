e08c5d211bfa19ede12c45f43b2533c8629e8882
Zhania Satubaldeeva <zhaniasatubaldeeva@yandex.ru>
2022-11-24 09:45:05 +0300

    Feat(Laravel): add changed vite.config file for future rebase

--- counter-cal-app/vite.config.js
+++ counter-cal-app/vite.config.js
@@ -4,7 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/script.js', 'resources/js/app.js','resources/css/welcome.css'],
+            input: ['resources/css/app.css', 'resources/js/script.js', 'resources/js/app.js','resources/css/welcome.css','resources/sass/app.scss',
+                'resources/css/style.css',
+                'resources/jquery/jquery.min.js',
+                'resources/jquery/jquery-ui.min.js',
+                'resources/js/main.js',],
             refresh: true,
         }),
     ],
